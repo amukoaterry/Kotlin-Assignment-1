d3a29d9766bcfdbab5fe06a46145a36855f1a678
amukoaterry <amukoaterrymitchell@gmail>
2024-02-21 00:56:17 +0300

    added one file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var studentname1 ="Terry Mitchell"
+    println(studentname1)
+
+    var age ="21"
+    println(age)
+
+    var phonenumber ="0748375701"
+    println(phonenumber)
+
+    var weightinkg =(78)
+    println(weightinkg)
+
+    var citizen =(true)
+    println(citizen)
+
+}
